149ea4709407cb0608348aa5ab90143373c9ce30
Wiseman Fernandes <wiseman.fernandes@kodework.com>
2017-03-16 15:51:21 +0530

    Script
    Realign for "&" and timeline's black line

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 $(function(){
-main = $("div.roundedOne");
-for (i = 0; i <= main.length - 1; i++) {
-	if (i == 0) {
-		$('canvas').drawLine({
-		  strokeStyle: '#000',
-		  strokeWidth: 5,
-		  x1: 15, y1: 0,
-		  x2: 15, y2: main.eq(6).offset().top + 90
-		});
-		continue
-	}
-	if (i == 6) {
+	$(".top-gray i").offset({
+		top: $(".top-gray-left h1").offset().top, 
+		left: $(document).width() / 2
+	}).removeClass("hide");
+	main = $("div.roundedOne");
+	for (i = 0; i <= main.length - 1; i++) {
+		if (i == 0) {
+			continue
+		}
+		if (i == 6) {
+			main.eq(i).offset({
+				top : main.eq(i - 1).offset().top + 220,
+				left : main.eq(i - 1).offset().left
+			});
+			continue
+		}
 		main.eq(i).offset({
-			top : main.eq(i - 1).offset().top + 220,
+			top : main.eq(i - 1).offset().top + 130,
 			left : main.eq(i - 1).offset().left
 		});
-		continue
 	}
-	main.eq(i).offset({
-		top : main.eq(i - 1).offset().top + 130,
-		left : main.eq(i - 1).offset().left
+	$('canvas').drawLine({
+	  strokeStyle: '#000',
+	  strokeWidth: 5,
+	  x1: 15, y1: 0,
+	  x2: 15, y2: main.eq(6).position().top - 200 
 	});
-}
-for (i = $("div.skills div p").length - 1; i >= 0; i--) {
-	$("div.skills div p").eq(i).width($("div.skills div").eq(i).attr("precent") + "%");
-}
+	for (i = $("div.skills div p").length - 1; i >= 0; i--) {
+		$("div.skills div p").eq(i).width($("div.skills div").eq(i).attr("precent") + "%");
+	}
 })
